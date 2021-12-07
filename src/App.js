1ca098ae7c9bae5081dcd9c9f64684d5c8034a19
Amrita Matta <matta.amrita@gmail.com>
2021-12-07 21:48:44 +0200

    added basic input form with display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    role: "",
+    message: "",
+  };
+
+  inputHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="firstName">First name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          onChange={this.inputHandler}
+        />
+        <label htmlFor="lastName">Last name:</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          onChange={this.inputHandler}
+        />
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          onChange={this.inputHandler}
+        />
+
+        <label htmlFor="role">Role:</label>
+        <select name="role" id="role" onChange={this.inputHandler}>
+          <option value="">Choose role</option>
+          <option value="teacher">Teacher</option>
+          <option value="developer">Developer</option>
+          <option value="scrumaster">Scrum Master</option>
+        </select>
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          rows="4"
+          cols="50"
+          placeholder="Add message here.."
+          onChange={this.inputHandler}
+        ></textarea>
+
+        <h1>Firstname={this.state.firstName}</h1>
+        <h1>LastName={this.state.lastName}</h1>
+        <h1>PhoneNumber={this.state.phoneNumber}</h1>
+        <h1>Role={this.state.role}</h1>
+        <h1>Message={this.state.message}</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
