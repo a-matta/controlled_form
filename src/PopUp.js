a48682b09ba53f0571b713c01be07b9a0423c80d
Amrita Matta <matta.amrita@gmail.com>
2021-12-08 19:02:18 +0200

    wrapped components as div

--- src/PopUp.js
+++ src/PopUp.js
@@ -13,7 +13,7 @@ export function PopUp({
   popUpConfirmationHandler,
 }) {
   return (
-    <>
+    <div>
       <h1>Firstname={firstName}</h1>
       <h1>LastName={lastName}</h1>
       <h1>PhoneNumber={phoneNumber}</h1>
@@ -21,6 +21,6 @@ export function PopUp({
       <h1>Message={message}</h1>
       <button onClick={popUpConfirmationHandler}>Yes, I am sure</button>
       <button onClick={closeHandler}>No, I don't want</button>
-    </>
+    </div>
   );
 }
--- src/View.js
+++ src/View.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export function View({ firstName, lastName, phoneNumber, role, message }) {
   return (
-    <>
+    <div>
       <h1>Firstname={firstName}</h1>
       <h1>LastName={lastName}</h1>
       <h1>PhoneNumber={phoneNumber}</h1>
       <h1>Role={role}</h1>
       <h1>Message={message}</h1>
-    </>
+    </div>
   );
 }
