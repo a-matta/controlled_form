9936ae61d9687cc35ae02b61cb4fb9549cedf815
Amrita Matta <matta.amrita@gmail.com>
2021-12-11 20:38:29 +0200

    popup

--- src/PopUp.js
+++ src/PopUp.js
@@ -14,27 +14,34 @@ export function PopUp({
 }) {
   return (
     <div className="overlay">
-      <div className="popup"></div>
-      <p>Your Note is here:</p>
-      <p>
-        Firstname=<span>{firstName}</span>
-      </p>
-      <p>
-        LastName=<span>{lastName}</span>
-      </p>
-      <p>
-        PhoneNumber=<span>{phoneNumber}</span>
-      </p>
-      <p>
-        Role=<span>{role}</span>
-      </p>
-      <p>
-        Message=<span>{message} </span>
-      </p>
-      <button onClick={popUpConfirmationHandler}>Yes, I am sure</button>
-      <button className="popupsecond" onClick={closeHandler}>
-        No, I don't want
-      </button>
+      <div className="popup">
+        <p>Your Note is here</p>
+        <p>
+          Firstname=<span>{firstName}</span>
+        </p>
+        <p>
+          LastName=<span>{lastName}</span>
+        </p>
+        <p>
+          PhoneNumber=<span>{phoneNumber}</span>
+        </p>
+        <p>
+          Role=<span>{role}</span>
+        </p>
+        <p>
+          Message=<span>{message} </span>
+        </p>
+        <div className="popup-close-button">&times;</div>
+        <button
+          className="popup-confirm-button"
+          onClick={popUpConfirmationHandler}
+        >
+          Yes, I am sure
+        </button>
+        <button className="popup-deny-button" onClick={closeHandler}>
+          No, I don't want
+        </button>
+      </div>
     </div>
   );
 }
